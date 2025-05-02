d97798fdb4af54bec375ee3aff63d7c123c39b77
AvhiMaz <avhimazumder5@outlook.com>
2025-05-02 14:33:20 +0530

    add nvim configs
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

---
+++ nvim-config/lua/plugins/init.lua
@@ -0,0 +1,68 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
+    'rust-lang/rust.vim',
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+    end
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+
+   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+    ensure_installed = {
+   		"vim", "lua", "vimdoc",
+      "html", "css", "javascript", "typescript", "tsx", "rust", "o"
+   		},
+   	},
+    highlight = { enable = true },
+   },
+
+}
