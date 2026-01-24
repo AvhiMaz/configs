eef8559d636bdce46f3b5636ecc6f8784034ede4
AvhiMaz <avhimazumder5@outlook.com>
2026-01-24 16:01:47 +0530

    update nvim
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- mac/nvim/lua/plugins/init.lua
+++ mac/nvim/lua/plugins/init.lua
@@ -102,4 +102,17 @@ return {
       },
     },
   },
+
+  -- Fugitive: Git wrapper
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse" },
+    keys = {
+      { "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
+      { "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
+      { "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
+      { "<leader>gl", "<cmd>Git pull<cr>", desc = "Git pull" },
+      { "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff" },
+    },
+  },
 }
